c0fcf37431555e3f2da28fe78744d782c3ae7fb6
tahafkh <fakharian.taha@ut.ac.ir>
2024-11-09 22:39:34 +0330

    Fix another bug

--- lip_reading.py
+++ lip_reading.py
@@ -221,6 +221,7 @@ else:
 			model_state_dict[new_key] = model_state_dict.pop(key)
 	
 	model.load_state_dict(model_state_dict, strict=True)
+	model.eval()
 	if args.round:
 		model.round_pos()
 	print(f'Total number of parameters: {sum(p.numel() for p in model.parameters())}')
